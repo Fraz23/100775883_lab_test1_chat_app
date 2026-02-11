b8b7f58f33d1bf7e7749c0960032ce6e2810492b
Faraz S <63739465+Fraz23@users.noreply.github.com>
2026-02-11 12:37:19 -0500

    Add Mongoose schemas

---
+++ models/PrivateMessage.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const privateMessageSchema = new mongoose.Schema({
+  from_user: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  to_user: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  message: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  date_sent: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("PrivateMessage", privateMessageSchema);
